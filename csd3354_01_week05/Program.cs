524d31673e3447c7c57cfa5c57d590c2ef950b27
visualstudio <visualstudio@122-PC37>
2019-02-08 21:08:04 -0500

    8:45 commit

--- csd3354_01_week05/Program.cs
+++ csd3354_01_week05/Program.cs
@@ -1,81 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace csd3354_01_week05
+
+
+class Department
+
+
+
 {
 
-    class Program
+
+
+    public Department(string dept_name)
+
+
 
     {
 
-        static void Main(string[] args)
-
-        {
-
-            Elevator e = new Elevator();
-
-            e.setup();
-
-            e.TraverseList();
-
-        }
-
-    }
 
 
-
-    class Node
-
-    {
-
-        public Node() { }
-
-        public Node elevatorUp;
-
-        public string FloorNumber;
-
-    }
-
-
-
-    class Department
-
-    {
-
-        public Department(string dept_name)
-
-        {
-
-            DepartmentDescription = dept_name;
-
-        }
-
-        public Department aDepartment;
-
-        public string DepartmentDescription;
-
-    }
-
-
-
-    class DepartmentStore
-
-    {
-
-        public void InitializeDepartments()
-
-        {
-
-            Department Kitchenware = new Department("Kitchenware");
-
-            Department Books = new Department("Books");
-
-
-
-        }
+        DepartmentDescription = dept_name;
 
 
 
@@ -83,86 +26,232 @@ namespace csd3354_01_week05
 
 
 
-    class Elevator
+    public Department aDepartment;
+
+
+
+
+
+
+
+    public string DepartmentDescription;
+
+
+
+}
+
+
+
+
+
+
+
+class DepartmentStore
+
+
+
+{
+
+
+
+    public void InitializeDepartments()
+
+
 
     {
 
-        Node Head;
 
-        Node FirstFloor;
 
-        Node SecondFloor;
-
-        Node ThirdFloor;
-
-        Node FourthFloor;
+        Department Kitchenware = new Department("Kitchenware");
 
 
 
-        public void setup()
-
-        {
-
-            FirstFloor = new Node();
-
-            SecondFloor = new Node();
-
-            ThirdFloor = new Node();
-
-            FourthFloor = new Node();
-
-            Head = FirstFloor;
-
-            FirstFloor.FloorNumber = "First Floor";
-
-            FirstFloor.elevatorUp = SecondFloor;
-
-            SecondFloor.FloorNumber = "Second Floor";
-
-            SecondFloor.elevatorUp = ThirdFloor;
-
-            ThirdFloor.FloorNumber = "Third Floor";
-
-            ThirdFloor.elevatorUp = FourthFloor;
-
-            FourthFloor.FloorNumber = "Fourth Floor";
-
-            FourthFloor.elevatorUp = null;
-
-        }
+        Department Books = new Department("Books");
 
 
 
-        public void TraverseList()
-
-        {
-
-            Node temp;
-
-            temp = Head;
 
 
 
-            // where am I going to start?
-
-
-
-            while (temp != null)
-
-            {
-
-                Console.WriteLine(temp.FloorNumber);
-
-                temp = temp.elevatorUp;
-
-
-
-            }
-
-
-
-        }
 
     }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+class Elevator
+
+
+
+{
+
+
+
+    Node Head;
+
+
+
+    Node FirstFloor;
+
+
+
+    Node SecondFloor;
+
+
+
+    Node ThirdFloor;
+
+
+
+    Node FourthFloor;
+
+
+
+
+
+
+
+    public void setup()
+
+
+
+    {
+
+
+
+        FirstFloor = new Node();
+
+
+
+        SecondFloor = new Node();
+
+
+
+        ThirdFloor = new Node();
+
+
+
+        FourthFloor = new Node();
+
+
+
+        Head = FirstFloor;
+
+
+
+        FirstFloor.FloorNumber = "First Floor";
+
+
+
+        FirstFloor.elevatorUp = SecondFloor;
+
+
+
+        SecondFloor.FloorNumber = "Second Floor";
+
+
+
+        SecondFloor.elevatorUp = ThirdFloor;
+
+
+
+        ThirdFloor.FloorNumber = "Third Floor";
+
+
+
+        ThirdFloor.elevatorUp = FourthFloor;
+
+
+
+        FourthFloor.FloorNumber = "Fourth Floor";
+
+
+
+        FourthFloor.elevatorUp = null;
+
+
+
+    }
+
+
+
+
+
+
+
+    public void TraverseList()
+
+
+
+    {
+
+
+
+        Node temp;
+
+
+
+        temp = Head;
+
+
+
+
+
+
+
+        // where am I going to start?
+
+
+
+
+
+
+
+        while (temp != null)
+
+
+
+        {
+
+
+
+            Console.WriteLine(temp.FloorNumber);
+
+
+
+            temp = temp.elevatorUp;
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
 
 }
